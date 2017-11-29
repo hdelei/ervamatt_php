3b1eb343c34a25ce018c3799528c72972cb1ce9a
VanderleiLab <hdelei@gmail.com>
2017-11-29 14:18:30 -0200

    Pequenas melhoris

--- phpaux/adm_galeria.php
+++ phpaux/adm_galeria.php
@@ -4,10 +4,19 @@
 
 $directory = "../img/agenda/";
 $images = glob($directory . "*.{jpg,png,gif,JPG,PNG,GIF}", GLOB_BRACE);
-array_splice($images, 12);
+//array_splice($images, 12);
+
+$start = 3;
+$move = "next";
+
+
+//
+if($move == "next" && $start > -1){
+	$newImages = array_slice($images, $start, 8);
+}
 
 if($images){
-	foreach($images as $image)
+	foreach($newImages as $image)
 	{
 		$img_name = utf8_encode(basename($image));
 		echo '<div class="sl_thumb"><img class="thumb_a" src="/img/agenda/' 
