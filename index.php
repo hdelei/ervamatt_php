1586e8963c3f2cbd53c71ce1340abe9974791b33
hdelei <hdelei@gmail.com>
2018-02-12 16:51:41 -0200

    Updated soundcloud list

--- index.php
+++ index.php
@@ -171,7 +171,8 @@
         </div>
             <div class="playlist" style="text-align:center;margin:auto; max-width:700px; height:365px;">
                 
-                <iframe id="souncloudframe" style="margin:auto; display:block; width:100%;"  height="362" scrolling="no" frameborder="no"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/301895548&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false"></iframe></div>
+                <iframe id="souncloudframe" style="margin:auto; display:block; width:100%;"  height="362" scrolling="no" 
+                frameborder="no"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/442299696&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false"></iframe></div>
             
         </div>
         
